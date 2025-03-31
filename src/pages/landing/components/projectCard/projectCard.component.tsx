4156c54dcdcda38ad865d22f49f9dcf1438c53ea
Alejo-develop <yeyito872@gmail.com>
2025-03-31 18:32:42 -0500

    feat: :lipstick: i'd add first steps on the Project card, only the body is finished

---
+++ src/pages/landing/components/projectCard/projectCard.component.tsx
@@ -0,0 +1,18 @@
+import "./projectCard.css";
+
+const ProjectCardComponent = () => {
+  return (
+    <div className="project-card">
+      <p className="project-name">VorteXtream</p>
+
+      <div className="project-section-info">
+        <div className="sectionPreview-project-Card"></div>
+        <div className="sectionInfo-Project-card"></div>
+      </div>
+
+      <p>git</p>
+    </div>
+  );
+};
+
+export default ProjectCardComponent;
--- src/pages/landing/sectionComponents/projects.components.tsx
+++ src/pages/landing/sectionComponents/projects.components.tsx
@@ -1,7 +1,13 @@
+import ProjectCardComponent from "../components/projectCard/projectCard.component";
+
 const ProjectsSectionComponent = () => {
   return (
     <div className="projects-div" id="projects">
       <h1 className="projects-title">PROJECTS</h1>
+
+      <div className="container-projects">
+        <ProjectCardComponent />
+      </div>
     </div>
   );
 };
