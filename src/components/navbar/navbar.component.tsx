cc04ed3cb17e8c73d3bad0f6f17d6d877cd267c3
Alejo-develop <yeyito872@gmail.com>
2025-04-07 13:27:32 -0500

    feat: :construction: i added a animation to show items and the portfolio, and i've implemented the animation on the header, missing other sections

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,35 +1,61 @@
-import { faGithub, faInstagram, faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 import "./navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import RevealComponent from "../reveal/reveal.component";
 
 const NavbarComponent = () => {
   return (
     <div className="navbar-socialMedia-section">
       <div></div>
       <nav className="div-nav">
-        <a href="#aboutme" className="item-nav">
-          ABOUT ME
-        </a>
-        <a href="#myskills" className="item-nav">
-          MY SKILLS
-        </a>
-        <a href="#projects" className="item-nav">
-          PROJECTS
-        </a>
+        <RevealComponent width="fit-content" animationColor={false}>
+          <a href="#aboutme" className="item-nav">
+            ABOUT ME
+          </a>
+        </RevealComponent>
+        <RevealComponent width="fit-content" animationColor={false}>
+          <a href="#myskills" className="item-nav">
+            MY SKILLS
+          </a>
+        </RevealComponent>
+        <RevealComponent width="fit-content" animationColor={false}>
+          <a href="#projects" className="item-nav">
+            PROJECTS
+          </a>
+        </RevealComponent>
       </nav>
       <div className="div-social-media">
-        <a href="https://wa.me/+573243165096" target="_blank">
-          <FontAwesomeIcon icon={faWhatsapp} className="icon"/>
-        </a>
-        <a href="https://github.com/Alejo-develop" target="_blank">
-          <FontAwesomeIcon icon={faGithub} className="icon"/>
-        </a>
-        <a href="https://www.linkedin.com/in/alejandro-vergara-zapata-267238213/" target="_blank">
-          <FontAwesomeIcon icon={faLinkedin} className="icon"/>
-        </a>
-        <a href="https://www.instagram.com/a.lej.o?igsh=MTdtbDNsYXZpZXF1Nw==" target="_blank">
-          <FontAwesomeIcon icon={faInstagram} className="icon"/>
-        </a>
+        <RevealComponent width="fit-content" animationColor={false}>
+          <a href="https://wa.me/+573243165096" target="_blank">
+            <FontAwesomeIcon icon={faWhatsapp} className="icon" />
+          </a>
+        </RevealComponent>
+        <RevealComponent width="fit-content" animationColor={false}>
+          <a href="https://github.com/Alejo-develop" target="_blank">
+            <FontAwesomeIcon icon={faGithub} className="icon" />
+          </a>
+        </RevealComponent>
+        <RevealComponent width="fit-content" animationColor={false}>
+          <a
+            href="https://www.linkedin.com/in/alejandro-vergara-zapata-267238213/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} className="icon" />
+          </a>
+        </RevealComponent>
+        <RevealComponent width="fit-content" animationColor={false}>
+          <a
+            href="https://www.instagram.com/a.lej.o?igsh=MTdtbDNsYXZpZXF1Nw=="
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faInstagram} className="icon" />
+          </a>
+        </RevealComponent>
       </div>
     </div>
   );
--- src/pages/landing/sectionComponents/header.component.tsx
+++ src/pages/landing/sectionComponents/header.component.tsx
@@ -1,5 +1,6 @@
 import { TypeAnimation } from "react-type-animation";
 import NavbarComponent from "../../../components/navbar/navbar.component";
+import RevealComponent from "../../../components/reveal/reveal.component";
 
 const HeaderCompoent = () => {
   return (
@@ -7,21 +8,32 @@ const HeaderCompoent = () => {
       <NavbarComponent />
       <div className="container-header">
         <div className="container-info-header">
-          <h1 className="title-header">Alejo-Develop</h1>
-          <h4 className="subtitle-header">Fullstack Developer</h4>
+          <RevealComponent width="fit-content">
+            <h1 className="title-header">Alejo-Develop</h1>
+          </RevealComponent>
+          <RevealComponent width="fit-content">
+            <h4 className="subtitle-header">Fullstack Developer</h4>
+          </RevealComponent>
           <div style={{ textAlign: "center" }}>
-            <p className="text-header">If you can think it...</p>
-            <TypeAnimation
-              sequence={["you can work it", 2000, "you can program it", 2000]}
-              wrapper="span"
-              speed={30}
-              repeat={Infinity}
-              className="text-header"
-            />
+            <RevealComponent width="fit-content">
+              <p className="text-header">If you can think it...</p>
+            </RevealComponent>
+
+            <RevealComponent width="fit-content">
+              <TypeAnimation
+                sequence={["you can work it", 2000, "you can program it", 2000]}
+                wrapper="span"
+                speed={30}
+                repeat={Infinity}
+                className="text-header"
+              />
+            </RevealComponent>
           </div>
         </div>
         <div className="container-img-header">
-          <div className="box-img"></div>
+          <RevealComponent width="fit-content">
+            <div className="box-img"></div>
+          </RevealComponent>
         </div>
       </div>
     </div>
